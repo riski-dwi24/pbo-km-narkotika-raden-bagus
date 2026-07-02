7a23d59284a9a59620ffda4db895d512be968651
riski-dwi24 <riskidwikirana@gmail.com>
2026-07-02 23:35:11 +0700

    feat: menambahkan validasi input angka pada InputHandler

--- src/util/InputHandler.java
+++ src/util/InputHandler.java
@@ -1,4 +1,16 @@
 package util;
 
+import java.util.Scanner;
+
 public class InputHandler {
-}
+    public static int validasiInt(String prompt, Scanner sc) {
+        while(true) {
+            try {
+                System.out.print(prompt);
+                String input = sc.nextLine().trim();
+                return Integer.parseInt(input);
+            } catch (NumberFormatException var3) {
+                System.out.println("  Error: Input harus berupa angka bulat. Silakan coba lagi.");
+            }
+        }
+    }
