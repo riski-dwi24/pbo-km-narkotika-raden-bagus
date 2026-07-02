2347a4ed130d88adbbd58e35255fda87d78178f3
riski-dwi24 <riskidwikirana@gmail.com>
2026-07-03 00:19:32 +0700

    feat: menambahkan interface Sortable untuk pengurutan data putusan

--- src/util/Sortable.java
+++ src/util/Sortable.java
@@ -1,4 +1,12 @@
 package util;
 
+import java.util.ArrayList;
+import model.Putusan;
+
 public interface Sortable {
+    ArrayList<Putusan> sortByVonis(ArrayList<Putusan> var1);
+
+    ArrayList<Putusan> sortByDenda(ArrayList<Putusan> var1);
+
+    ArrayList<Putusan> sortByNama(ArrayList<Putusan> var1);
 }
